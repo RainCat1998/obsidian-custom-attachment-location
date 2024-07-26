4d662e7349a96732453d72f37b7d223797772589
Michael Naumov <mnaoumov@gmail.com>
2024-07-25 22:02:26 -0600

    Remove TemplateString

--- src/TemplateString.ts
+++
@@ -1,9 +0,0 @@
-export default class TemplateString extends String {
-  public interpolate(params: object): string {
-    const names = Object.keys(params);
-    const values: unknown[] = Object.values(params);
-    const str = this as unknown as string;
-    // eslint-disable-next-line @typescript-eslint/no-implied-eval
-    return new Function(...names, `return \`${str}\`;`)(...values) as string;
-  }
-}

2ffce463bbf0ba1422df559e04744fbbcfde82d3
Michael Naumov <mnaoumov@gmail.com>
2024-07-28 20:52:16 -0600

    Fix script

--- scripts/npm/spellcheck.ts
+++ scripts/npm/spellcheck.ts
@@ -1,5 +1,5 @@
 import { execFromRoot } from "../tools/root.ts";
 
-export default function spellcheck(): void {
+export default async function spellcheck(): Promise<void> {
   await execFromRoot("npx cspell . --no-progress");
 }

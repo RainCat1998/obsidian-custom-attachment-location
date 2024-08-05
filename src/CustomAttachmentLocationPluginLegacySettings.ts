b1ab5c9a2db097e1e2cd7b60db0c1823472a1e6f
Michael Naumov <mnaoumov@gmail.com>
2024-08-05 15:40:21 -0600

    Use inheritance

--- src/CustomAttachmentLocationPluginLegacySettings.ts
+++ src/CustomAttachmentLocationPluginLegacySettings.ts
@@ -1,17 +1,6 @@
-export default class CustomAttachmentLocationPluginLegacySettings {
-  public attachmentFolderPath: string = "./assets/${filename}";
-  public autoRenameFiles: boolean = false;
-  public autoRenameFolder: boolean = true;
-  public convertImagesToJpeg: boolean = false;
-  public convertImagesOnDragAndDrop: boolean = false;
+import CustomAttachmentLocationPluginSettings from "./CustomAttachmentLocationPluginSettings.ts";
+
+export default class CustomAttachmentLocationPluginLegacySettings extends CustomAttachmentLocationPluginSettings {
   public dateTimeFormat: string = "";
-  public duplicateNameSeparator: string = " ";
-  public jpegQuality: number = 0.8;
-  public keepEmptyAttachmentFolders: boolean = false;
-  public pastedFileName: string = "file-${date:YYYYMMDDHHmmssSSS}";
   public pastedImageFileName: string = "";
-  public renamePastedFilesWithKnownNames: boolean = false;
-  public renameAttachmentsOnDragAndDrop: boolean = false;
-  public replaceWhitespace: boolean = false;
-  public toLowerCase: boolean = false;
 }

5d71e99d19be2c133768cae559cbc2ffef0577b5
Michael Naumov <mnaoumov@gmail.com>
2024-12-09 00:48:02 -0600

    Remove date selector. Refactor templating

--- src/AttachmentPath.ts
+++ src/AttachmentPath.ts
@@ -1,8 +1,5 @@
-import type { TAbstractFile } from 'obsidian';
-
+import { replaceAllAsync, trimStart } from 'obsidian-dev-utils/String';
 import moment from 'moment';
-import { TFolder } from 'obsidian';
-import { throwExpression } from 'obsidian-dev-utils/Error';
 import { prompt } from 'obsidian-dev-utils/obsidian/Modal/Prompt';
 import { escapeRegExp } from 'obsidian-dev-utils/RegExp';
 
@@ -11,86 +8,57 @@ import type { Substitutions } from './Substitutions.ts';
 
 import { validateFilename } from './PathValidator.ts';
 import { createSubstitutionsFromPath } from './Substitutions.ts';
+import { join } from 'obsidian-dev-utils/Path';
+import { normalizePath } from 'obsidian';
 
 export async function getAttachmentFolderFullPathForPath(plugin: CustomAttachmentLocationPlugin, path: string): Promise<string> {
   return await getAttachmentFolderPath(plugin, createSubstitutionsFromPath(path));
 }
 
 export async function getAttachmentFolderPath(plugin: CustomAttachmentLocationPlugin, substitutions: Substitutions): Promise<string> {
-  return await getEarliestAttachmentFolder(plugin, plugin.settingsCopy.attachmentFolderPath, substitutions);
-}
-
-export async function getEarliestAttachmentFolder(plugin: CustomAttachmentLocationPlugin, attachmentFolderTemplate: string, substitutions: Substitutions): Promise<string> {
-  const app = plugin.app;
-  const targetRegex = interpolateToDigitRegex(attachmentFolderTemplate, substitutions);
-  const folders = app.vault.getAllLoadedFiles()
-    .filter((f: TAbstractFile) => f instanceof TFolder)
-    .filter((f: TAbstractFile) => targetRegex.test(f.path));
-
-  interface FolderStat {
-    ctime: number;
-    path: string;
-  }
-
-  const folderStats: FolderStat[] = [];
-
-  for (const folder of folders) {
-    const stat = await app.vault.adapter.stat(folder.path);
-    folderStats.push({
-      ctime: stat?.ctime ?? 0,
-      path: folder.path
-    });
-  }
-
-  if (folderStats.length > 0) {
-    // create time ascending
-    return folderStats.sort((a, b) => a.ctime - b.ctime).map((f) => f.path)[0] ?? throwExpression(new Error('No folder stat'));
-  } else {
-    return interpolateDateToString(plugin, attachmentFolderTemplate, substitutions);
-  }
+  return await resolvePathTemplate(plugin, plugin.settingsCopy.attachmentFolderPath, substitutions);
 }
 
 export async function getPastedFileName(plugin: CustomAttachmentLocationPlugin, substitutions: Substitutions): Promise<string> {
-  return await interpolateDateToString(plugin, plugin.settingsCopy.pastedFileName, substitutions);
+  return await resolvePathTemplate(plugin, plugin.settingsCopy.pastedFileName, substitutions);
 }
 
-/**
- * example:
- *   /dir1/${date:YYYY}/${date:MM}/${filename} -> /dir1/2024/06/targetFileName
- * @param template template path contains meta vars
- * @returns interpolate vars begin with ${date:**} (moment.js format) and ${filename} to string path, using now time
- */
-export async function interpolateDateToString(plugin: CustomAttachmentLocationPlugin, template: string, substitutions: Substitutions): Promise<string> {
-  // match ${date:date_format} pattern
-  const dateRegExp = /\$\{date:(.*?)\}/g;
-
-  let newPath = template.replaceAll(dateRegExp, (_, dateFormat: string) => moment().format(dateFormat));
-
-  for (const [key, value] of Object.entries(substitutions) as [string, string][]) {
-    newPath = newPath.replaceAll(`\${${key}}`, value);
-  }
-
-  if (substitutions.originalCopiedFilename) {
-    if (newPath.includes('${prompt}')) {
-      const newFileName = await prompt({
+export async function resolvePathTemplate(plugin: CustomAttachmentLocationPlugin, template: string, substitutions: Substitutions): Promise<string> {
+  let resolvedPath = await replaceAllAsync(template, /\${(.+?)}/g, async (_: string, key: string) => {
+    if (key.startsWith('date:')) {
+      const format = trimStart(key, 'date:');
+      return moment().format(format);
+    }
+    if (key in substitutions) {
+      return substitutions[key as keyof Substitutions];
+    }
+    if (key === 'prompt') {
+      const promptResult = await prompt({
         app: plugin.app,
         defaultValue: substitutions.originalCopiedFilename,
-        title: 'Rename attachment file',
-        valueValidator: (value): string => {
-          return validateFilename(value);
-        }
-      }) ?? substitutions.originalCopiedFilename;
-      newPath = newPath.replaceAll('${prompt}', newFileName);
+        title: 'Provide a value for ${prompt} template',
+        valueValidator: validateFilename
+      });
+      if (promptResult === null) {
+        throw new Error('Prompt cancelled');
+      }
+      return promptResult;
     }
-  }
+
+    throw new Error(`Unknown key: ${key}`);
+  });
 
   if (plugin.settingsCopy.toLowerCase) {
-    newPath = newPath.toLowerCase();
+    resolvedPath = resolvedPath.toLowerCase();
   }
 
-  newPath = replaceWhitespace(plugin, newPath);
-  newPath = newPath.replace(/\.\//, substitutions.folderPath + '/');
-  return newPath;
+  resolvedPath = replaceWhitespace(plugin, resolvedPath);
+  if (resolvedPath.startsWith('./')) {
+    resolvedPath = join(substitutions.folderPath, resolvedPath);
+  }
+
+  resolvedPath = normalizePath(resolvedPath);
+  return resolvedPath;
 }
 
 /**
